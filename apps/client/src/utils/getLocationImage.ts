8fc5037c303b833067c483c8900a7fefcaeb74ce
Vlad <vlad070210@gmail.com>
2026-08-24 06:04:13 -0700

    локации в бд

--- apps/client/src/utils/getLocationImage.ts
+++ apps/client/src/utils/getLocationImage.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:4000";
+const API_URL = import.meta.env.VITE_API_BASE_URL ?? "";
 
 export function getLocationImage(fileName: string) {
   return `${API_URL}/location-images/${fileName}`;
