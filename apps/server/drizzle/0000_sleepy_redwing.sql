38f539b8295beb96a5062169e7730c4c8b44ed1c
Vlad <vlad070210@gmail.com>
2026-08-24 04:28:14 -0700

    локации в бд

--- apps/server/drizzle/0000_sleepy_redwing.sql
+++ apps/server/drizzle/0000_sleepy_redwing.sql
@@ -25,7 +25,9 @@ CREATE TABLE `locations` (
 	`name` text NOT NULL,
 	`description` text NOT NULL,
 	`x` real NOT NULL,
-	`y` real NOT NULL
+	`y` real NOT NULL,
+	`homeImg`text NOT NULL,
+	`fightImg`text NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `players` (
