31b964ae95f23c922659b5000129532424804f17
Матвей <vvhh3144@gmail.com>
2026-08-24 17:45:50 +0400

    test5

--- apps/client/src/utils/getLocationImage.ts
+++ apps/client/src/utils/getLocationImage.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_BASE_URL 
+const API_URL = "https://mmotgbot-production.up.railway.app"
 
 export function getLocationImage(fileName: string) {
   return `${API_URL}/location-images/${fileName}`
